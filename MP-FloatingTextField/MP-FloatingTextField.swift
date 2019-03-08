c2cdd21ac06e88992f1e149e102cf0dce3e02b0a
Manos Psarakis <psarakism@gmail.com>
2019-03-08 09:42:41 +0200

    Added test cases

---
+++ MP-FloatingTextField/MP-FloatingTextField.swift
@@ -0,0 +1,25 @@
+//
+//  MP-FloatingTextField.swift
+//  MP-FloatingTextField
+//
+//  Created by Manos Psarakis on 08/03/2019.
+//  Copyright © 2019 Psarakis. All rights reserved.
+//
+
+import Foundation
+
+
+public final class MPFloatingTextField {
+    
+    
+    let name = "MP-FloatingTextField"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+
+}
