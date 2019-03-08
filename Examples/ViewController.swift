1cb3b2721032f44443a2741b67cc5c580c00c1bb
Manos Psarakis <psarakism@gmail.com>
2019-03-08 08:27:43 +0200

    initial commit 2

---
+++ Examples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examples
+//
+//  Created by Manos Psarakis on 08/03/2019.
+//  Copyright © 2019 Psarakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MP-FloatingTextField/MP_FloatingTextField.h
@@ -0,0 +1,19 @@
+//
+//  MP_FloatingTextField.h
+//  MP-FloatingTextField
+//
+//  Created by Manos Psarakis on 08/03/2019.
+//  Copyright © 2019 Psarakis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MP_FloatingTextField.
+FOUNDATION_EXPORT double MP_FloatingTextFieldVersionNumber;
+
+//! Project version string for MP_FloatingTextField.
+FOUNDATION_EXPORT const unsigned char MP_FloatingTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MP_FloatingTextField/PublicHeader.h>
+
+
